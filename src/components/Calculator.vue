1216f9d8ae3e73c024a0ece756e81698c8a509ee
SmartWise <wisdomandy.5@gmail.com>
2021-01-21 16:32:29 +0000

    My second commit

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,32 +1,124 @@
 <template>
   <div class="calculator">
-    <div class="display">0.15478</div>
-    <div class="btn">C</div>
-    <div class="btn">+/-</div>
-    <div class="btn">%</div>
-    <div class="btn operator">/</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
-    <div class="btn operator">*</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
-    <div class="btn operator">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
-    <div class="btn operator">+</div>
-    <div class="btn zero">0</div>
-    <div class="btn">.</div>
-    <div class="btn operator">=</div>
+    <div class="display">{{ valueOnScreen || '0' }}</div>
+    <div @click="clear" class="btn">C</div>
+    <div @click="sign" class="btn">+/-</div>
+    <div @click="percentage" class="btn">%</div>
+    <div @click="division" class="btn operator">/</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
+    <div @click="multiplication" class="btn operator">*</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
+    <div @click="substraction" class="btn operator">-</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
+    <div @click="addition" class="btn operator">+</div>
+    <div @click="append('0')" class="btn zero">0</div>
+    <div @click="decimal" class="btn">.</div>
+    <div @click="equal" class="btn operator">=</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Calculator',
-}
+
+  data(){
+    return{
+      valueOnScreen: '0',
+      operator: null,
+      stamp: null,
+      clicked: false
+    }
+  },
+
+  methods:{
+    clear(){
+      this.valueOnScreen = ''
+      this.operator= null
+      this.stamp= null
+      this.clicked=false
+    },
+
+    sign(){
+      this.valueOnScreen = (this.valueOnScreen.charAt(0) === '-') ?
+          `${this.valueOnScreen.slice(1)}` : `-${this.valueOnScreen}`
+    },
+
+    percentage(){
+      this.valueOnScreen = `${parseFloat(this.valueOnScreen)/100}`
+    },
+
+    append(payload){
+      if (this.clicked){
+        this.valueOnScreen = ''
+        this.clicked = false
+      }
+      if (this.valueOnScreen === '0'){
+        this.valueOnScreen = payload
+      }else{
+        this.valueOnScreen += payload
+      }
+    },
+
+    decimal(){
+      if (this.valueOnScreen.indexOf('.') === -1){
+        this.append('.')
+      }
+    },
+
+    changeState(){
+      if (this.operator !== null)
+        this.equal()
+      this.stamp = this.valueOnScreen
+      this.clicked = true
+    },
+
+    division(){
+      this.changeState()
+      this.operator = "divide"
+    },
+
+    multiplication(){
+      this.changeState()
+      this.operator = "times"
+    },
+
+    substraction(){
+      this.changeState()
+      this.operator = "minus"
+    },
+
+    addition(){
+      this.changeState()
+      this.operator = "add"
+    },
+
+    equal(){
+      if (this.stamp !== null ){
+        switch (this.operator){
+          case "add":
+            this.valueOnScreen = `${parseFloat(this.stamp) + parseFloat(this.valueOnScreen)}`
+              break;
+            case "minus":
+              this.valueOnScreen = `${parseFloat(this.stamp) - parseFloat(this.valueOnScreen)}`
+              break;
+          case "times":
+            this.valueOnScreen = `${parseFloat(this.stamp) * parseFloat(this.valueOnScreen)}`
+            break;
+          case "divide":
+            this.valueOnScreen = `${parseFloat(this.stamp) / parseFloat(this.valueOnScreen)}`
+            break;
+        }
+      }
+    }
+  }
+
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -37,15 +129,15 @@ export default {
   grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: minmax(50px, auto);
   font-size: 40px;
-  width: 45%;
   margin: auto;
 }
 
 .display{
-  grid-column: 1/5;
   background-color: #333;
+  grid-column: 1/5;
   color: white;
   padding: 15px;
+  text-align: right;
 }
 
 .zero{
